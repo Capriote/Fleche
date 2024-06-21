52fbc382e121d372abf20ef198c33acd8f596105
Isaac McQueen <imcqueen@truehomesusa.com>
2024-06-21 17:01:44 -0400

    Add nix flake for devshell

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [ bun ];
+        shellHook = '''';
+      };
+    };
+}
